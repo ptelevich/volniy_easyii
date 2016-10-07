cc2fafd2c87f81cfd50e3588628b685304fdc7cc
ptelevich <pavel.televic@teqniksoft.com>
2016-10-07 17:46:33 +0300

    add links for get token

--- vkModules/helpers/Auth.php
+++ vkModules/helpers/Auth.php
@@ -27,6 +27,8 @@ class Auth
         $this->vkObject = new VKontakte();
     }
 
+    // https://oauth.vk.com/authorize?client_id=3682744&scope=wall,offline,audio&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.7&response_type=token
+    // https://oauth.vk.com/authorize?client_id=3087106&scope=wall,offline,audio&redirect_uri=https://oauth.vk.com/blank.html&display=page&v=5.7&response_type=token
     protected function makeOAuth()
     {
         $this->vkObject->clientId = $this->clientId;
@@ -38,7 +40,7 @@ class Auth
             //$this->vkObject->setAccessToken(null); exit;
             //$this->vkObject->setAccessToken((new OAuthToken)->setToken('ed0a039e6ea0aac9e3ef62289e2470c34d9f6cf303b784a5fde5061a3d3b433dd7bdb8cbbb4752ce71f3c'));
             $auth = new OAuthToken();
-            $auth->setToken('2dd2778883ac834f1972c7f80f3576ebcbf1d5f69521fec05158c62a9d083535273f5f6b4cdcd95960fe7');
+            $auth->setToken('280f86c739e82ff4e18403685be6a3e5feaa09f61e217fa2a331291f0e8b28a301fd1dc7c8f483feee86e');
             $this->vkObject->setAccessToken($auth);
             $accessToken = $this->vkObject->getAccessToken();
             $requestCode = Yii::$app->request->get('code', null);
