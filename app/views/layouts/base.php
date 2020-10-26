1abcd5666036c40db88989dd4e29273c17f437fe
PavelT <pavel@volniy.by>
2020-10-26 23:24:49 +0300

    Added layout and uploads

--- app/views/layouts/base.php
+++ app/views/layouts/base.php
@@ -8,6 +8,7 @@ $asset = \app\assets\AppAsset::register($this);
     <head>
         <meta charset="<?= Yii::$app->charset ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="yandex-verification" content="e57bb707eabc4a2b" />
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
